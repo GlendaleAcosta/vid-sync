7225c3f9a5a8027eb610fc98a11f3b5ee2e1fe65
Glendale Acosta <gglendale17@gmail.com>
2019-01-17 03:18:19 -0800

    edited package.json

---
+++ build/precache-manifest.957e69df2afcd26c4ccbd770223199d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb17cfcfe57309b91fb174da5070f631",
+    "url": "/static/media/yourface.fb17cfcf.png"
+  },
+  {
+    "revision": "2dbf63fb4767dd68dd13012e6922f803",
+    "url": "/static/media/Rectangle.2dbf63fb.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a482605fe584ffafd118",
+    "url": "/static/js/main.a482605f.chunk.js"
+  },
+  {
+    "revision": "fc26fb3f102d3fe659b4",
+    "url": "/static/js/1.fc26fb3f.chunk.js"
+  },
+  {
+    "revision": "a482605fe584ffafd118",
+    "url": "/static/css/main.6a9754f0.chunk.css"
+  },
+  {
+    "revision": "fc26fb3f102d3fe659b4",
+    "url": "/static/css/1.fd606da8.chunk.css"
+  },
+  {
+    "revision": "821a07ecab6f5b5f9106d5b1852eaa26",
+    "url": "/index.html"
+  }
+];
